c8446e745f65f15a1668625252d8837deac79a2b
clientweb35-dotcom <clientweb35@gmail.com>
2026-06-26 12:44:22 +0530

    Add files via upload

---
+++ firebase-config (8).js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCoLdvr_dSf7Mk_QGsE8OLHyXEsOLT9sl0",
+  authDomain: "clientweb35-452f0.firebaseapp.com",
+  databaseURL: "https://clientweb35-452f0-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "clientweb35-452f0",
+  storageBucket: "clientweb35-452f0.firebasestorage.app",
+  messagingSenderId: "1030694417955",
+  appId: "1:1030694417955:web:b75b87714d1dbc41bf6396",
+  measurementId: "G-T373JTT86E"
+};
+
+const IMGBB_API_KEY = "9cc7e5238b6f6df040f3cd1e93b5e92a";
+const CLOUDINARY_CLOUD = "djcjvxcep";
+const CLOUDINARY_PRESET = "clientweb35";
